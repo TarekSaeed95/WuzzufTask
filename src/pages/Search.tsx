52c111d7d8b1f668bf71f048546e5589c3425ed6
tarek.fakhr <tarek.fakhr@pharmacymarts.com>
2024-08-24 20:05:24 +0300

    add no result

--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -14,13 +14,20 @@ export const Search = () => {
   const jobsCards = jobs.map((job) => (
     <JobCardComponent key={job.id} job={job} />
   ));
-  if (isLoadingJobsSearch) return <Loader className="mt-100" />;
+
   return (
     <section className="search-page">
       <SearchBar />
       <div className="container">
-        <h1 className="title">{`"${searchTerm}" jobs (${meta.count})`}</h1>
-        <div className="jobs-container">{jobsCards}</div>
+        {isLoadingJobsSearch && <Loader className="mt-100" />}
+        {jobsCards.length ? (
+          <>
+            <h1 className="title">{`"${searchTerm}" jobs (${meta.count})`}</h1>
+            <div className="jobs-container">{jobsCards}</div>
+          </>
+        ) : (
+          <p className="no-result">No Results</p>
+        )}
       </div>
     </section>
   );

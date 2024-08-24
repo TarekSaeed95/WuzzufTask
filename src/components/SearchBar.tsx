8c12ae4ae69161203e1489cfe7a887d130579f74
tarek.fakhr <tarek.fakhr@pharmacymarts.com>
2024-08-24 07:11:41 +0300

    add search bar functionallity

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,10 +1,30 @@
 import { FaSearch } from "react-icons/fa";
+import { setSearchTerm } from "../redux/features";
+import { useAppDispatch, useAppSelector } from "../hooks";
+import { useLocation, useNavigate } from "react-router-dom";
 
 export const SearchBar = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
+  const searchTerm = useAppSelector((state) => state.search.searchTerm);
+  const dispatch = useAppDispatch();
+  const changeInputHandler = (term: string) => {
+    dispatch(setSearchTerm(term));
+    if (term.length > 2 && location.pathname !== "/jobs/search") {
+      navigate("/jobs/search");
+    }
+  };
   return (
     <section className="search-section">
       <div className="search-container">
-        <input type="text" placeholder="search keyword" />
+        <input
+          value={searchTerm}
+          onChange={(e) => {
+            changeInputHandler(e.target.value);
+          }}
+          type="text"
+          placeholder="search keyword"
+        />
         <FaSearch />
       </div>
     </section>

9a0caba2ab688ecaad53752718a5d383d4c245a9
tarek.fakhr <tarek.fakhr@pharmacymarts.com>
2024-08-24 19:16:38 +0300

    add debouncing search

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,24 +1,32 @@
 import { FaSearch } from "react-icons/fa";
 import { setSearchTerm } from "../redux/features";
-import { useAppDispatch, useAppSelector } from "../hooks";
+import { useAppDispatch } from "../hooks";
 import { useLocation, useNavigate } from "react-router-dom";
-
+import { useCallback, useState } from "react";
+import _debounce from "lodash/debounce";
 export const SearchBar = () => {
+  const [value, setValue] = useState("");
   const navigate = useNavigate();
   const location = useLocation();
-  const searchTerm = useAppSelector((state) => state.search.searchTerm);
   const dispatch = useAppDispatch();
   const changeInputHandler = (term: string) => {
-    dispatch(setSearchTerm(term));
-    if (term.length > 2 && location.pathname !== "/jobs/search") {
+    setValue(term);
+    debounceFn(term);
+  };
+
+  const handleDebounceFn = (inputValue: string) => {
+    dispatch(setSearchTerm(inputValue));
+    if (inputValue.length > 2 && location.pathname !== "/jobs/search") {
       navigate("/jobs/search");
     }
   };
+
+  const debounceFn = useCallback(_debounce(handleDebounceFn, 500), []);
   return (
     <section className="search-section">
       <div className="search-container">
         <input
-          value={searchTerm}
+          value={value}
           onChange={(e) => {
             changeInputHandler(e.target.value);
           }}
